fb591b6ddc2f4ce72bad61bccc49297a58ffa62f
Karthick <karthick@adhisoftware.co.in>
2015-12-22 12:01:37 +0530

    wk_user_leave

--- app/models/wk_user_leave.rb
+++ app/models/wk_user_leave.rb
@@ -1,3 +1,8 @@
 class WkUserLeave < ActiveRecord::Base
   unloadable
+include Redmine::SafeAttributes
+
+  belongs_to :user
+  belongs_to :issue, :class_name => 'Issue', :foreign_key => 'issue_id'
+  
 end
