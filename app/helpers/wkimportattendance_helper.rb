75a7b8f80355118b6e6f3c728aeca96ca776b456
Karthick <karthick@adhisoftware.co.in>
2016-08-17 14:04:12 +0530

    Show error msg when fields in file not configured

--- app/helpers/wkimportattendance_helper.rb
+++ app/helpers/wkimportattendance_helper.rb
@@ -23,86 +23,93 @@ module WkimportattendanceHelper
 		if !(csv.length > 0)
 			@errorMsg = l('error_no_record_to_import')
 		end
-		csv.each_with_index do |row,index|
-			# rowValueHash - Have the data of the current row
-			rowValueHash = Hash.new
-			columnArr.each_with_index do |col,i|
-				case col when "user_id"
-					rowValueHash["user_id"] = row[i]
-				when "start_time","end_time"
-					if row_date(row[i]).is_a?(DateTime) || (row[i].blank? && col == "end_time")
-						rowValueHash[col] = row_date(row[i]) 
+		if columnArr.blank? || !(columnArr.length > 0)
+			@errorMsg = l('warning_fields_in_file_not_configured')
+		end
+		if @errorMsg.blank?
+			csv.each_with_index do |row,index|
+				# rowValueHash - Have the data of the current row
+				rowValueHash = Hash.new
+				columnArr.each_with_index do |col,i|
+					case col when "user_id"
+						rowValueHash["user_id"] = row[i]
+					when "start_time","end_time"
+						if row_date(row[i]).is_a?(DateTime) || (row[i].blank? && col == "end_time")
+							rowValueHash[col] = row_date(row[i]) 
+						else
+							#isValid = false
+							@errorHash[index+1] = col + " " + l('activerecord.errors.messages.invalid')
+						end
+					when "hours"
+						rowValueHash[col] = row[i].to_f
 					else
-						#isValid = false
-						@errorHash[index+1] = col + " " + l('activerecord.errors.messages.invalid')
-					end
-				when "hours"
-					rowValueHash[col] = row[i].to_f
-				else
-					if index < 1
-						cfId = col.to_i #userCFHash[col] 
-						userIdCFHash = getUserIdCFHash(cfId)
-					end
-					if userIdCFHash[row[i]].blank?
-						@errorHash[index+1] = userCFHash[col.to_i] + " " + l('activerecord.errors.messages.invalid')
-					else
-						rowValueHash["user_id"] = userIdCFHash[row[i]]	
-					end
-				end
-			end
-			# Check the row has any invalid entries and skip that row from import
-			if @errorHash[index+1].blank?
-				@importCount = @importCount + 1
-				userId = rowValueHash["user_id"] #row[0].to_i
-				endEntry = nil
-				startEntry = getFormatedTimeEntry(rowValueHash["start_time"])
-				if !rowValueHash["end_time"].blank? && (rowValueHash["end_time"] != rowValueHash["start_time"]) 
-					endEntry = getFormatedTimeEntry(rowValueHash["end_time"]) 
-				end
-				
-				if (columnArr.include? "end_time") && (columnArr.include? "start_time")
-					#Get the imported records for particular user and start_time
-					importedEntry = WkAttendance.where(:user_id => userId, :start_time => startEntry)
-					if  importedEntry[0].blank? 
-						# There is no records for the given user on the given start_time
-						# Insert a new record to the database
-						lastAttnEntriesHash[userId] = addNewAttendance(startEntry,endEntry,userId)
-					else
-						# Update the record with end Entry
-						if importedEntry[0].end_time.blank? && !endEntry.blank?
-							lastAttnEntriesHash[userId] = saveAttendance(importedEntry[0], startEntry, endEntry, userId, true)
+						if index < 1
+							cfId = col.to_i #userCFHash[col] 
+							userIdCFHash = getUserIdCFHash(cfId)
+						end
+						if userIdCFHash[row[i]].blank?
+							@errorHash[index+1] = userCFHash[col.to_i] + " " + l('activerecord.errors.messages.invalid')
+						else
+							rowValueHash["user_id"] = userIdCFHash[row[i]]	
 						end
 					end
-				else
-					# Get the imported records for particular user and entry_time
-					# Skip the records which is already inserted by check importedEntry[0].blank? 
-					importedEntry = WkAttendance.where("user_id = ? AND (start_time = ? OR end_time = ?)", userId, startEntry, startEntry)
-					if importedEntry[0].blank? 
-						lastAttnEntriesHash[userId] = saveAttendance(lastAttnEntriesHash[userId], startEntry, endEntry, userId, false)	
+				end
+				# Check the row has any invalid entries and skip that row from import
+				if @errorHash[index+1].blank?
+					@importCount = @importCount + 1
+					userId = rowValueHash["user_id"] #row[0].to_i
+					endEntry = nil
+					startEntry = getFormatedTimeEntry(rowValueHash["start_time"])
+					if !rowValueHash["end_time"].blank? && (rowValueHash["end_time"] != rowValueHash["start_time"]) 
+						endEntry = getFormatedTimeEntry(rowValueHash["end_time"]) 
+					end
+					
+					if (columnArr.include? "end_time") && (columnArr.include? "start_time")
+						#Get the imported records for particular user and start_time
+						importedEntry = WkAttendance.where(:user_id => userId, :start_time => startEntry)
+						if  importedEntry[0].blank? 
+							# There is no records for the given user on the given start_time
+							# Insert a new record to the database
+							lastAttnEntriesHash[userId] = addNewAttendance(startEntry,endEntry,userId)
+						else
+							# Update the record with end Entry
+							if importedEntry[0].end_time.blank? && !endEntry.blank?
+								lastAttnEntriesHash[userId] = saveAttendance(importedEntry[0], startEntry, endEntry, userId, true)
+							end
+						end
+					else
+						# Get the imported records for particular user and entry_time
+						# Skip the records which is already inserted by check importedEntry[0].blank? 
+						importedEntry = WkAttendance.where("user_id = ? AND (start_time = ? OR end_time = ?)", userId, startEntry, startEntry)
+						if importedEntry[0].blank? 
+							lastAttnEntriesHash[userId] = saveAttendance(lastAttnEntriesHash[userId], startEntry, endEntry, userId, false)	
+						end
 					end
 				end
 			end
-		end
-		Rails.logger.info("==== #{l(:notice_import_finished, :count => @importCount)} ====")
-		Rails.logger.info("==== #{l(:notice_import_finished_with_errors, :count => @errorHash.count, :total => (@errorHash.count + @importCount))} ====")
-		if isAuto
-			Rails.logger.info("====== File Name = #{File.basename file}=========")
-			if  @importCount > 0
-				Rails.logger.info("==== #{l(:notice_import_finished, :count => @importCount)} ====")
-			end
-			if !@errorHash.blank? && @errorHash.count > 0
-				Rails.logger.info("==== #{l(:notice_import_finished_with_errors, :count => @errorHash.count, :total => (@errorHash.count + @importCount))} ====")
-				Rails.logger.info("===============================================================")
-				Rails.logger.info("       Row           ||        Message            ")
-				Rails.logger.info("===============================================================")
-				@errorHash.each do |item|
-					Rails.logger.info("    #{item[0]}           || #{simple_format_without_paragraph item[1]}")
-					Rails.logger.info("---------------------------------------------------------------")
+			Rails.logger.info("==== #{l(:notice_import_finished, :count => @importCount)} ====")
+			Rails.logger.info("==== #{l(:notice_import_finished_with_errors, :count => @errorHash.count, :total => (@errorHash.count + @importCount))} ====")
+			if isAuto
+				Rails.logger.info("====== File Name = #{File.basename file}=========")
+				if  @importCount > 0
+					Rails.logger.info("==== #{l(:notice_import_finished, :count => @importCount)} ====")
 				end
-				Rails.logger.info("===============================================================")
+				if !@errorHash.blank? && @errorHash.count > 0
+					Rails.logger.info("==== #{l(:notice_import_finished_with_errors, :count => @errorHash.count, :total => (@errorHash.count + @importCount))} ====")
+					Rails.logger.info("===============================================================")
+					Rails.logger.info("       Row           ||        Message            ")
+					Rails.logger.info("===============================================================")
+					@errorHash.each do |item|
+						Rails.logger.info("    #{item[0]}           || #{simple_format_without_paragraph item[1]}")
+						Rails.logger.info("---------------------------------------------------------------")
+					end
+					Rails.logger.info("===============================================================")
+				end
 			end
+			return @errorHash.blank? || @errorHash.count < 0
+		else
+			return false
 		end
-		return @errorHash.blank? || @errorHash.count < 0
 	end
 	
 	def read_file(file)
