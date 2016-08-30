0417a90b7245f5c812d5b8512fce6ca0a61af4ce
Karthick <karthick@adhisoftware.co.in>
2016-08-30 11:27:33 +0530

    Modify wk_salary table to wk_salaries
    remove is_deleted column

--- db/migrate/004_create_wk_salary_components.rb
+++ db/migrate/004_create_wk_salary_components.rb
@@ -17,22 +17,21 @@ class CreateWkSalaryComponents < ActiveRecord::Migration
 	  t.references :salary_component, :class => "WkSalaryComponents", :null => false
 	  t.references :dependent, :class => "WkSalaryComponents", :null => true
       t.float :factor
-	  t.string :is_deleted, :null => false, :limit => 1, :default => 'N'
 	  t.timestamps null: false
     end
 	add_index  :wk_user_salary_components, :user_id
 	add_index  :wk_user_salary_components, :salary_component_id
 	
-	create_table :wk_salary do |t|
+	create_table :wk_salaries do |t|
       t.references :user, :null => false
 	  t.references :salary_component, :class => "WkSalaryComponents", :null => false
       t.float :amount
       t.date :salary_date
 	  t.timestamps null: false
     end
-	add_index  :wk_salary, :user_id
-	add_index  :wk_salary, :salary_component_id
-	add_index  :wk_salary, :salary_date
+	add_index  :wk_salaries, :user_id
+	add_index  :wk_salaries, :salary_component_id
+	add_index  :wk_salaries, :salary_date
 
   end
 end
