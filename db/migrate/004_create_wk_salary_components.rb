c27dcd0470d54bb9bf823bfd5287a821948ec21d
Karthick <karthick@adhisoftware.co.in>
2016-09-07 16:06:48 +0530

    Modify and create history tables

--- db/migrate/004_create_wk_salary_components.rb
+++ db/migrate/004_create_wk_salary_components.rb
@@ -4,9 +4,8 @@ class CreateWkSalaryComponents < ActiveRecord::Migration
       t.string :name
       t.string :frequency, :null => true, :limit => 3
       t.date :start_date
-      t.string :type, :null => false, :limit => 3
+      t.string :component_type, :null => false, :limit => 3
 	  t.references :dependent, :class => "WkSalaryComponents", :null => true
-      t.string :pay_period, :null => true, :limit => 3
       t.float :factor
       t.string :salary_type, :null => true, :limit => 3
 	  t.timestamps null: false
@@ -27,11 +26,21 @@ class CreateWkSalaryComponents < ActiveRecord::Migration
 	  t.references :salary_component, :class => "WkSalaryComponents", :null => false
       t.float :amount
       t.date :salary_date
+	  t.column :currency, :string, :limit => 5, :default => '$'
 	  t.timestamps null: false
     end
 	add_index  :wk_salaries, :user_id
 	add_index  :wk_salaries, :salary_component_id
 	add_index  :wk_salaries, :salary_date
+	
+	create_table :wk_h_user_salary_components do |t|
+      t.references :user, :null => false
+	  t.references :user_salary_component, :class => "WkUserSalaryComponents", :null => false
+	  t.references :salary_component, :class => "WkSalaryComponents", :null => false
+	  t.references :dependent, :class => "WkSalaryComponents", :null => true
+      t.float :factor
+	  t.timestamps null: false
+    end
 
   end
 end
