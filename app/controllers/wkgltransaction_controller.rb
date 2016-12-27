110b00cad1864d7a788e1f225f13014963aeaa66
dhineshrajasekar <dhineshryes@gmail.com>
2016-12-27 17:36:30 +0530

    bugFixing: transaction amt save on debit when amount 0

--- app/controllers/wkgltransaction_controller.rb
+++ app/controllers/wkgltransaction_controller.rb
@@ -59,7 +59,7 @@ class WkgltransactionController < WkaccountingController
 						
 					end
 					wktxnDetail.ledger_id = params["txn_particular#{i}"]
-					if params["txn_debit#{i}"].blank?
+					if params["txn_debit#{i}"].blank? || params["txn_debit#{i}"].to_i == 0
 						wktxnDetail.detail_type = 'c'
 						wktxnDetail.amount = params["txn_credit#{i}"]
 					else
