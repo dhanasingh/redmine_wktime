fc7ffdd8126aebd224977023c6dfdb4f857359da
Karthick <karthick@adhisoftware.co.in>
2016-12-28 10:47:47 +0530

    Remove unwanted destroy for transDetail

--- app/controllers/wkgltransaction_controller.rb
+++ app/controllers/wkgltransaction_controller.rb
@@ -60,7 +60,6 @@ class WkgltransactionController < WkaccountingController
 					end
 					wktxnDetail.ledger_id = params["txn_particular#{i}"]
 					if (params["txn_debit#{i}"].blank? || params["txn_debit#{i}"].to_i == 0) && (params["txn_credit#{i}"].blank? || params["txn_credit#{i}"].to_i == 0)
-						wktxnDetail.destroy
 						next
 					elsif params["txn_debit#{i}"].blank? || params["txn_debit#{i}"].to_i == 0
 						wktxnDetail.detail_type = 'c'
