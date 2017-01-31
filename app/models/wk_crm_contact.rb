59531e3599ccdc164581b258d50535c14ab75e22
Karthick <karthick@adhisoftware.co.in>
2017-01-31 13:40:08 +0530

    Show Fullname for contact

--- app/models/wk_crm_contact.rb
+++ app/models/wk_crm_contact.rb
@@ -24,4 +24,66 @@ class WkCrmContact < ActiveRecord::Base
   has_one :contact, foreign_key: 'contact_id', class_name: 'WkCrmContact'
   has_many :activities, as: :parent, class_name: 'WkCrmActivity'
   validates_presence_of :last_name
+   # Different ways of displaying/sorting users
+  NAME_FORMATS = {
+    :firstname_lastname => {
+        :string => '#{first_name} #{last_name}',
+        :order => %w(first_name last_name id),
+        :setting_order => 1
+      },
+    :firstname_lastinitial => {
+        :string => '#{first_name} #{last_name.to_s.chars.first}.',
+        :order => %w(first_name last_name id),
+        :setting_order => 2
+      },
+    :firstinitial_lastname => {
+        :string => '#{first_name.to_s.gsub(/(([[:alpha:]])[[:alpha:]]*\.?)/, \'\2.\')} #{last_name}',
+        :order => %w(first_name last_name id),
+        :setting_order => 2
+      },
+    :first_name => {
+        :string => '#{first_name}',
+        :order => %w(first_name id),
+        :setting_order => 3
+      },
+    :lastname_firstname => {
+        :string => '#{last_name} #{first_name}',
+        :order => %w(last_name first_name id),
+        :setting_order => 4
+      },
+    :lastnamefirstname => {
+        :string => '#{last_name}#{first_name}',
+        :order => %w(last_name first_name id),
+        :setting_order => 5
+      },
+    :lastname_comma_firstname => {
+        :string => '#{last_name}, #{first_name}',
+        :order => %w(last_name first_name id),
+        :setting_order => 6
+      },
+    :last_name => {
+        :string => '#{last_name}',
+        :order => %w(last_name id),
+        :setting_order => 7
+      },
+    :username => {
+        :string => '#{login}',
+        :order => %w(login id),
+        :setting_order => 8
+      },
+  }
+  
+  # Return user's full name for display
+  def name(formatter = nil)
+    f = self.class.name_formatter(formatter)
+    if formatter
+      eval('"' + f[:string] + '"')
+    else
+      @name ||= eval('"' + f[:string] + '"')
+    end
+  end
+
+  def self.name_formatter(formatter = nil)
+    NAME_FORMATS[formatter] || NAME_FORMATS[:firstname_lastname]
+  end
 end
