f8b42884c75cbd3ceaa3d900e9c3c3dff0c07add
Karthick <karthickmadhiazhagan@gmail.com>
2017-05-16 18:49:42 +0530

    Fix for duplicate credits

--- app/helpers/wkinvoice_helper.rb
+++ app/helpers/wkinvoice_helper.rb
@@ -551,8 +551,9 @@ include WkbillingHelper
 		group by i.id) inv
 		left join (select sum(amount) paid_amount, invoice_id from wk_payment_items group by invoice_id) pay on pay.invoice_id = inv.id
 		left join wk_payment_items pit on(inv.id = pit.invoice_id)
-		left join (select sum(amount) given_pay_credit, credit_payment_item_id from wk_invoice_items
-		where credit_payment_item_id is not null group by credit_payment_item_id) pcr on (pcr.credit_payment_item_id = pit.id)
+		left join (select gcr.*, invitm.invoice_id from (select sum(amount) given_pay_credit, credit_payment_item_id from wk_invoice_items
+		where credit_payment_item_id is not null group by credit_payment_item_id) gcr
+		left join wk_payment_items invitm on (invitm.id = gcr.credit_payment_item_id)) pcr on (pcr.credit_payment_item_id = pit.id OR  pcr.invoice_id = inv.id)
 		left join (select sum(amount) given_inv_credit, credit_invoice_id from wk_invoice_items
 		where credit_invoice_id is not null group by credit_invoice_id) icr on (icr.credit_invoice_id = inv.id)
 		left join wk_invoices i on i.id = inv.id
