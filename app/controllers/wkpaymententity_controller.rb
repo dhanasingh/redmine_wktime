b11f2d3a502309d693920a18ede6e4c3138e84f5
dhineshrajasekar <dhineshryes@gmail.com>
2017-06-21 16:24:28 +0530

    retrieve_date_range move to base class

--- app/controllers/wkpaymententity_controller.rb
+++ app/controllers/wkpaymententity_controller.rb
@@ -98,16 +98,16 @@ class WkpaymententityController < WkbillingController
 	end
 
 	def set_filter_session
-        if params[:searchlist].blank? && session[:controller_name].nil?
-			session[:controller_name] = {:period_type => params[:period_type],:period => params[:period], :contact_id => params[:contact_id], :account_id => params[:account_id], :polymorphic_filter =>  params[:polymorphic_filter], :from => @from, :to => @to }
+        if params[:searchlist].blank? && session[controller_name].nil?
+			session[controller_name] = {:period_type => params[:period_type],:period => params[:period], :contact_id => params[:contact_id], :account_id => params[:account_id], :polymorphic_filter =>  params[:polymorphic_filter], :from => @from, :to => @to }
 		elsif params[:searchlist] == controller_name
-			session[:controller_name][:period_type] = params[:period_type]
-			session[:controller_name][:period] = params[:period]
-			session[:controller_name][:from] = params[:from]
-			session[:controller_name][:to] = params[:to]
-			session[:controller_name][:contact_id] = params[:contact_id]
-			session[:controller_name][:account_id] = params[:account_id]
-			session[:controller_name][:polymorphic_filter] = params[:polymorphic_filter]
+			session[controller_name][:period_type] = params[:period_type]
+			session[controller_name][:period] = params[:period]
+			session[controller_name][:from] = params[:from]
+			session[controller_name][:to] = params[:to]
+			session[controller_name][:contact_id] = params[:contact_id]
+			session[controller_name][:account_id] = params[:account_id]
+			session[controller_name][:polymorphic_filter] = params[:polymorphic_filter]
 		end
 		
     end
@@ -133,58 +133,6 @@ class WkpaymententityController < WkbillingController
 		@entry_count = entries.count
         setLimitAndOffset()
 		@payment_entries = entries.order(id: :desc).limit(@limit).offset(@offset)
-	end
-	
-	# Retrieves the date range based on predefined ranges or specific from/to param dates
-	def retrieve_date_range
-		@free_period = false
-		@from, @to = nil, nil
-		period_type = session[:controller_name][:period_type]
-		period = session[:controller_name][:period]
-		fromdate = session[:controller_name][:from]
-		todate = session[:controller_name][:to]
-		
-		if (period_type == '1' || (period_type.nil? && !period.nil?)) 
-		    case period.to_s
-			  when 'today'
-				@from = @to = Date.today
-			  when 'yesterday'
-				@from = @to = Date.today - 1
-			  when 'current_week'
-				@from = getStartDay(Date.today - (Date.today.cwday - 1)%7)
-				@to = @from + 6
-			  when 'last_week'
-				@from =getStartDay(Date.today - 7 - (Date.today.cwday - 1)%7)
-				@to = @from + 6
-			  when '7_days'
-				@from = Date.today - 7
-				@to = Date.today
-			  when 'current_month'
-				@from = Date.civil(Date.today.year, Date.today.month, 1)
-				@to = (@from >> 1) - 1
-			  when 'last_month'
-				@from = Date.civil(Date.today.year, Date.today.month, 1) << 1
-				@to = (@from >> 1) - 1
-			  when '30_days'
-				@from = Date.today - 30
-				@to = Date.today
-			  when 'current_year'
-				@from = Date.civil(Date.today.year, 1, 1)
-				@to = Date.civil(Date.today.year, 12, 31)
-	        end
-		elsif period_type == '2' || (period_type.nil? && (!fromdate.nil? || !todate.nil?))
-		    begin; @from = fromdate.to_s.to_date unless fromdate.blank?; rescue; end
-		    begin; @to = todate.to_s.to_date unless todate.blank?; rescue; end
-		    @free_period = true
-		else
-		  # default
-		  # 'current_month'		
-			@from = Date.civil(Date.today.year, Date.today.month, 1)
-			@to = (@from >> 1) - 1
-	    end    
-		
-		@from, @to = @to, @from if @from && @to && @from > @to
-
 	end
 	
 	def getBillableProjIds
