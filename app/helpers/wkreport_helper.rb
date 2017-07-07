1a2c8a4e56bc4742d13e7d91ab71f9ae4d2f3bb3
Karthick <karthickmadhiazhagan@gmail.com>
2017-07-07 18:30:57 +0530

    Fix for AR ageing and sales activity reports showing supplier entities

--- app/helpers/wkreport_helper.rb
+++ app/helpers/wkreport_helper.rb
@@ -144,7 +144,7 @@ module WkreportHelper
 		if ActiveRecord::Base.connection.adapter_name == 'Mysql2'
 			parentSql = "COLLATE utf8_unicode_ci"
 		end
-		sqlStr = "select 'WkAccount' #{parentSql} as parent_type, id as parent_id from wk_accounts union select 'WkCrmContact' #{parentSql} as parent_type, id as parent_id from wk_crm_contacts"
+		sqlStr = "select 'WkAccount' #{parentSql} as parent_type, id as parent_id from wk_accounts where account_type = 'A' union select 'WkCrmContact' #{parentSql} as parent_type, id as parent_id from wk_crm_contacts where contact_type = 'C'"
 		sqlStr
 	end
 
