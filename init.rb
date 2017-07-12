037a32e761fea3874278108796da0bcab12889a5
dhineshrajasekar <dhineshryes@gmail.com>
2017-07-12 11:38:24 +0530

    fix: expense report  error and change  location.

--- init.rb
+++ init.rb
@@ -555,13 +555,13 @@ class WktimeHook < Redmine::Hook::ViewListener
 	end
 	
 	# Added expense report link in redmine core 'projects/show.html' using hook
-	def view_projects_show_sidebar_bottom(context={})
+	def view_projects_show_left(context={})
 		if !context[:project].blank?
 			wktime_helper = Object.new.extend(WktimeHelper)		
 			host_with_subdir = wktime_helper.getHostAndDir(context[:request])	
 			project_ids = Setting.plugin_redmine_wktime['wkexpense_projects']		
 			if project_ids.blank? || (!project_ids.blank? && (project_ids == [""] || project_ids.include?("#{context[:project].id}"))) && User.current.allowed_to?(:view_time_entries, context[:project])
-				"#{link_to(l(:label_wkexpense_reports), url_for(:controller => 'wkexpense', :action => 'reportdetail', :project_id => context[:project], :host => host_with_subdir, :only_path => true))}"
+				"<p style='float:left; padding-left:180px;  margin-top:-45px;'>| #{link_to(l(:label_wkexpense_reports), url_for(:controller => 'wkexpense', :action => 'reportdetail', :project_id => context[:project], :host => host_with_subdir, :only_path => true))}</p>"
 			end
 		end
 	end
