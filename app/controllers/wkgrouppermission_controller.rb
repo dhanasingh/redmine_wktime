25a6ca79ff06b24ed340ddb33ccc99e20f3ac784
dhineshrajasekar <dhineshryes@gmail.com>
2017-10-09 18:27:27 +0530

    erpmine permission  group list

--- app/controllers/wkgrouppermission_controller.rb
+++ app/controllers/wkgrouppermission_controller.rb
@@ -3,7 +3,33 @@ class WkgrouppermissionController < ApplicationController
 
 
 
-  def index
-  end
+	def index
+		@groups =  nil
+		entries = Group.sorted
+		entries = entries.like(params[:name]) if params[:name].present?
+		formPagination(entries)
+	end
+  
+	def formPagination(entries)
+		@entry_count = entries.count
+		setLimitAndOffset()
+		@groups = entries.limit(@limit).offset(@offset)
+	end
+	
+	def setLimitAndOffset		
+		if api_request?
+			@offset, @limit = api_offset_and_limit
+			if !params[:limit].blank?
+				@limit = params[:limit]
+			end
+			if !params[:offset].blank?
+				@offset = params[:offset]
+			end
+		else
+			@entry_pages = Paginator.new @entry_count, per_page_option, params['page']
+			@limit = @entry_pages.per_page
+			@offset = @entry_pages.offset
+		end	
+	end
 
 end
