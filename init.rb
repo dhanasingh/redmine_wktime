d2607d1e58073ae981aafcb7ad86aab9d60c6704
dhineshrajasekar <dhineshryes@gmail.com>
2017-12-26 18:19:45 +0530

    Add plugin id for settingscontroller patch

--- init.rb
+++ init.rb
@@ -7,6 +7,7 @@ require 'time_report_patch'
 require_dependency 'timelog_helper_patch'
 require_dependency 'queries_helper_patch'
 require 'userscontroller_patch'
+require 'settingscontroller_patch'
 
 User.class_eval do
 	has_one :wk_user, :dependent => :destroy, :class_name => 'WkUser'
@@ -135,61 +136,6 @@ module IssuesControllerPatch
  end
 end
 
-module SettingsControllerPatch
-	def self.included(base)
-		base.class_eval do
-			def plugin
-				wkpayroll_helper = Object.new.extend(WkpayrollHelper)
-				settinghash = Hash.new()
-				payrollValues = Hash.new()
-				settinghash = params[:settings]		
-				if !settinghash.blank? 
-					payrollValues[:basic] = settinghash["wktime_payroll_basic"]
-					payrollValues[:allowances] = settinghash["wktime_payroll_allowances"]
-					payrollValues[:deduction] = settinghash["wktime_payroll_deduction"]			
-					payrollValues[:payroll_deleted_ids] = settinghash["payroll_deleted_ids"]
-					settinghash.delete("wktime_payroll_basic") 
-					settinghash.delete("wktime_payroll_allowances") 
-					settinghash.delete("wktime_payroll_deduction") 
-					settinghash.delete("payroll_deleted_ids")
-					params[:settings] = settinghash
-				end	
-				@plugin = Redmine::Plugin.find(params[:id])
-				unless @plugin.configurable?
-				  render_404
-				  return
-				end
-				
-				if request.post?
-				  Setting.send "plugin_#{@plugin.id}=", params[:settings]
-				  wkpayroll_helper.savePayrollSettings(payrollValues)
-				  flash[:notice] = l(:notice_successful_update)
-				  redirect_to plugin_settings_path(@plugin)
-				else
-				  @partial = @plugin.settings[:partial]			   
-				  @settings = Setting.send "plugin_#{@plugin.id}"				
-				  dep_list = WkSalaryComponents.order('name')
-				  basic = Array.new
-				  allowance = Array.new
-				  deduction = Array.new
-				  hashval = Hash.new()
-				  unless dep_list.blank?
-						dep_list.each do |list| 
-						basic = [list.id.to_s + '|' + list.name + '|' + list.salary_type + '|' + list.factor.to_s + '|' + list.ledger_id.to_s ]  if list.component_type == 'b'	
-						allowance = allowance << list.id.to_s + '|' + list.name+'|'+list.frequency.to_s+'|'+ (list.start_date).to_s+'|'+(list.dependent_id).to_s+'|'+list.factor.to_s + '|' + list.ledger_id.to_s	if list.component_type == 'a'
-						deduction = deduction << list.id.to_s + '|' + list.name + '|' + list.frequency.to_s + '|' + (list.start_date).to_s + '|' + (list.dependent_id).to_s + '|' + (list.factor).to_s + '|' + list.ledger_id.to_s if list.component_type == 'd'
-							
-						end
-					end
-					hashval["wktime_payroll_basic"] = basic
-					hashval["wktime_payroll_allowances"] = allowance
-					hashval["wktime_payroll_deduction"] = deduction
-					@settings.merge!(hashval)
-				end				
-			end
-		end
-	end
-end
   
 CustomFieldsHelper.send(:include, WktimeHelperPatch)
 ProjectsController.send(:include, ProjectsControllerPatch)
