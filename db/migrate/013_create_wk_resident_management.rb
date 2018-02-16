24ee4caaa2849fbe37f4e8f584afaf60ca460285
Karthick <karthickmadhiazhagan@gmail.com>
2018-02-16 13:50:10 +0530

    Column rename

--- db/migrate/013_create_wk_resident_management.rb
+++ db/migrate/013_create_wk_resident_management.rb
@@ -31,7 +31,7 @@ class CreateWkResidentManagement  < ActiveRecord::Migration
 			t.references :invoice_item, :class => "wk_invoice_items", :null => true, :index => true
 		end
 		
-		add_reference :wk_crm_contacts, :contact_id, :class => "wk_crm_contacts", :null => true, index: true
+		add_reference :wk_crm_contacts, :contact, :class => "wk_crm_contacts", :null => true, index: true
 		
 		reversible do |dir|
 			dir.up do
