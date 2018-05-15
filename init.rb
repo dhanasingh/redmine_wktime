11f53ce7b0864c5b2c7df8aaf453875a2b152f6a
dhinesh <dhineshryes@gmail.com>
2018-05-15 13:40:15 +0530

    Fix for WkIssues not getting updated

--- init.rb
+++ init.rb
@@ -21,6 +21,7 @@ Issue.class_eval do
 	has_one :wk_issue, :dependent => :destroy, :class_name => 'WkIssue'
 	has_many :assignees, :dependent => :destroy, :class_name => 'WkIssueAssignee'
 	accepts_nested_attributes_for :assignees
+	accepts_nested_attributes_for :wk_issue
 	def erpmineissues
 		self.wk_issue ||= WkIssue.new(:issue => self, :project => self.project)
 	end	
