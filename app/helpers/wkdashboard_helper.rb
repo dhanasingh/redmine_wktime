85042295fff0f4a6baca49e51a829b4e8e68ebf5
Mohan <nk.mohan96@gmail.com>
2019-01-02 12:10:31 +0530

    code cleanup
    Remove the commented dashboard controller methods

--- app/helpers/wkdashboard_helper.rb
+++ app/helpers/wkdashboard_helper.rb
@@ -4,28 +4,19 @@ module WkdashboardHelper
 	include WkreportHelper
 	include Redmine::I18n
   
-   def get_graphs_yaml_path
+  def get_graphs_yaml_path
 	 files_array = []
 	 Dir["plugins/redmine_wktime/app/views/wkdashboard/*.yml"].each do |f| 
 		file_name = File.basename(f)	
 		files_array << ["#{file_name}"] 
 	 end   
-   end
+  end
   
-   def label_check(l_name)     
+  def label_check(l_name)     
       I18n.t( l_name, default: l_name )
-   end
-	
-	# def get_graphs_with_type
-		# {"clock_in_users_over_time" => "Bar",
-		# "expense_for_issues" => "Pie",
-		# "lead_generation_vs_conversion" => "Line",
-		# "invoice_vs_payment_per_month" => "Line",
-		# "assests_per_month" => "Line",
-		# "profit_loss_per_month" => "Line"}
-	# end
-	
-	def options_for_period_select(value)
+  end
+
+  def options_for_period_select(value)
 		options_for_select([
 							[l(:label_this_week), 'current_week'],
 							[l(:label_last_week), 'last_week'],
@@ -33,10 +24,10 @@ module WkdashboardHelper
 							[l(:label_last_month), 'last_month'],
 							[l(:label_this_year), 'current_year']],
 							value.blank? ? 'current_week' : value)
-	end
+  end
 	
-	def showDashboard
+  def showDashboard
 		!Setting.plugin_redmine_wktime['wktime_enable_dashboards_module'].blank? &&
 			Setting.plugin_redmine_wktime['wktime_enable_dashboards_module'].to_i == 1
-	end
+  end
 end
