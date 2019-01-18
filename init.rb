2d3510928d9c8238cd4783503947148d5bc7d481
RubarajR <rubaraj1093@gmail.com>
2019-01-18 19:35:38 +0530

    update init.rb and readme doc

--- init.rb
+++ init.rb
@@ -538,7 +538,7 @@ Redmine::Plugin.register :redmine_wktime do
   name 'ERPmine'
   author 'Adhi Software Pvt Ltd'
   description 'ERPmine is an ERP for Service Industries. It has the following modules: Time & Expense, Attendance, Payroll, CRM, Billing, Accounting, Purchasing, Inventory, Asset , Reports and Dashboards'
-  version '3.4'
+  version '3.4.1'
   url 'http://www.redmine.org/plugins/wk-time'
   author_url 'http://www.adhisoftware.co.in/'
   
