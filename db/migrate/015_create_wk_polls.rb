69bef1030437eaaaa3d0752b7b80c21aa640d2fa
Mohan <nk.mohan96@gmail.com>
2019-02-05 12:38:44 +0530

    Poll migration changes

--- db/migrate/015_create_wk_polls.rb
+++ db/migrate/015_create_wk_polls.rb
@@ -3,15 +3,15 @@ class CreateWkPolls < ActiveRecord::Migration[4.2]
 
         create_table :wk_polls do |t|
             t.string :name, :null => false
-            t.string :type
+            t.string :types
             t.string :status, :null => false, :limit => 3, :default => 'O'
             t.boolean :in_active, :default => false
             t.timestamps null: false
         end
 
         create_table :wk_poll_questions do |t|
-            t.string :description, :null => false
-            t.string :type
+            t.string :name, :null => false
+            t.string :types
             t.references :poll, :class => "wk_polls", :null => false
             t.timestamps null: false
 
@@ -25,6 +25,7 @@ class CreateWkPolls < ActiveRecord::Migration[4.2]
         create_table :wk_poll_sel_choices do |t|
             t.references :user, :class => "User", :null => false
             t.references :poll_choice, :class => "wk_poll_choices", :null => false
+            t.string :ip_address
             t.timestamps null: false
         end
 
