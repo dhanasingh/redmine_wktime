8c62d4c26149fb05bb73fb6c4482556c383f32f0
Mohan <Mohan@adhi45.local>
2019-06-27 11:43:39 +0530

    Fixed invoice total issue
    Added amount total in invoice edit page
    Fixed page error issue in purchase order and supplier invoice

--- app/controllers/wkpurchaseorder_controller.rb
+++ app/controllers/wkpurchaseorder_controller.rb
@@ -31,7 +31,7 @@ class WkpurchaseorderController < WksupplierorderentityController
 			rfqQuotEntry = WkRfqQuote.where(:quote_id => params[:quote_id].to_i)
 			@rfqQuotObj = rfqQuotEntry.blank? || rfqQuotEntry[0].blank? ? nil : rfqQuotEntry[0]
 			if !params[:populate_items].blank? && params[:populate_items] == '1'
-				@invoiceItem = WkInvoiceItem.where(:invoice_id => @rfqQuotObj.quote_id).select(:name, :rate, :amount, :quantity, :item_type, :currency, :project_id, :modifier_id,  :invoice_id )
+				@invoiceItem = WkInvoiceItem.where(:invoice_id => @rfqQuotObj.quote_id).select(:name, :rate, :amount, :quantity, :item_type, :currency, :project_id, :modifier_id,  :invoice_id, :original_amount, :original_currency)
 			end		
 		end			
 	end
