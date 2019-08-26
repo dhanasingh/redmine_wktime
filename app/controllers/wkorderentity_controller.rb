2e316a493d4a43e1c085ab6fe622a41c4432e5c6
RubarajR <rubaraj1093@gmail.com>
2019-08-26 18:21:10 +0530

    Fixed duplicate issue in billing

--- app/controllers/wkorderentity_controller.rb
+++ app/controllers/wkorderentity_controller.rb
@@ -17,7 +17,6 @@ include WkgltransactionHelper
 					'invoice_date' => "invoice_date",
 					'status' => "wk_invoices.status",
 					'name' => "CASE WHEN wk_invoices.parent_type = 'WkAccount' THEN a.name ELSE CONCAT(c.first_name, c.last_name) END",
-					'project' => "projects.name",
 					'start_date' => "start_date",
 					'end_date' => "end_date",
 					'amount' => "amount",
@@ -135,11 +134,10 @@ include WkgltransactionHelper
 				invEntries = invEntries.where( :id => invIds)
 			end
 			invEntries = invEntries.joins("LEFT JOIN wk_invoice_items ON wk_invoice_items.invoice_id = wk_invoices.id
-				LEFT JOIN (SELECT id, name FROM projects) AS projects ON wk_invoice_items.project_id = projects.id
 				LEFT JOIN (SELECT id, firstname, lastname FROM users) AS users ON wk_invoices.modifier_id = users.id
 				LEFT JOIN wk_accounts a on (wk_invoices.parent_type = 'WkAccount' and wk_invoices.parent_id = a.id)
 				LEFT JOIN wk_crm_contacts c on (wk_invoices.parent_type = 'WkCrmContact' and wk_invoices.parent_id = c.id)
-				").group("wk_invoices.id, CASE WHEN wk_invoices.parent_type = 'WkAccount' THEN a.name ELSE CONCAT(c.first_name, c.last_name) END, projects.name,
+				").group("wk_invoices.id, CASE WHEN wk_invoices.parent_type = 'WkAccount' THEN a.name ELSE CONCAT(c.first_name, c.last_name) END,
 				CONCAT(users.firstname, users.lastname)")
 				.select("wk_invoices.*, SUM(wk_invoice_items.quantity) AS quantity, SUM(wk_invoice_items.amount) AS amount, SUM(wk_invoice_items.original_amount)
 				 AS original_amt")
