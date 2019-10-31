6e82efa07a19db3feb64a84a857d4c7db869921c
Mohan <nk.mohan96@gmail.com>
2019-10-31 10:40:43 +0530

    Fixed save documents error
    Without attachment error

--- app/controllers/wkdocument_controller.rb
+++ app/controllers/wkdocument_controller.rb
@@ -9,24 +9,28 @@ class WkdocumentController < WkbaseController
 
   def save
     errMsg = ''
-    params[:attachments].each do |atch_param|
-      attachment = Attachment.find_by_token(atch_param[1][:token])
-      next if attachment.blank?
-      attachment.container_type = params[:container_type]
-      attachment.container_id = params[:container_id]
-      attachment.filename = attachment.filename
-      attachment.description = atch_param[1][:description]
-      attachment.save
-      unless attachment.save
-        errMsg += attachment.errors.full_messages.to_s
+    if params[:attachments].present?
+      params[:attachments].each do |atch_param|
+        attachment = Attachment.find_by_token(atch_param[1][:token])
+        next if attachment.blank?
+        attachment.container_type = params[:container_type]
+        attachment.container_id = params[:container_id]
+        attachment.filename = attachment.filename
+        attachment.description = atch_param[1][:description]
+        attachment.save
+        unless attachment.save
+          errMsg += attachment.errors.full_messages.to_s
+        end
       end
+    else
+      errMsg = l(:error_invalid_document)
     end
     if errMsg.blank?
 		  redirect_to getRedirectUrl(params[:container_id], params[:container_type])
       flash[:notice] = l(:notice_successful_update)
     else
 			flash[:error] = errMsg
-      redirect_to action: 'edit', container_type: params[:container_type], container_id: params[:container_id]
+      redirect_to action: 'new', container_type: params[:container_type], container_id: params[:container_id]
     end
   end
 
