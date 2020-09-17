32fe637ca903d982efce643d813a003df3d6cd6b
Mohan <nk.mohan96@gmail.com>
2020-09-17 13:43:42 +0530

    Added map feature to assets

--- db/migrate/024_add_assets_map.rb
+++ db/migrate/024_add_assets_map.rb
@@ -1,7 +1,7 @@
 class AddAssetsMap < ActiveRecord::Migration[5.2]
   def change
     add_column :wk_leave_reqs, :reviewer_comment, :text
-    add_column :wk_inventory_items, :longitude, :decimal, precision: 30, scale: 20
-    add_column :wk_inventory_items, :latitude, :decimal, precision: 30, scale: 20
+    add_column :wk_asset_properties, :longitude, :decimal, precision: 30, scale: 20
+    add_column :wk_asset_properties, :latitude, :decimal, precision: 30, scale: 20
   end
 end
