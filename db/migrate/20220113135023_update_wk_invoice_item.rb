0b1f2b752cde335454eb80e85ced77e04443b8bc
RubarajR <40016173+RubarajR@users.noreply.github.com>
2022-01-28 20:06:14 +0530

    Updated Invoice Item Migration

--- db/migrate/20220113135023_update_wk_invoice_item.rb
+++ db/migrate/20220113135023_update_wk_invoice_item.rb
@@ -1,7 +1,7 @@
 class UpdateWkInvoiceItem < ActiveRecord::Migration[5.2]
   def change
     add_reference :wk_invoice_items, :invoice_item, polymorphic: true, index: true
-    add_reference :wk_invoice_items, :parent_id, class: "WkInvoiceItem"
+    add_reference :wk_invoice_items, :parent, class: "WkInvoiceItem"
     reversible do |dir|
       dir.up do
         change_column :wk_inventory_items, :notes, :text
