e72f1b5aab98849387be747c0e849382cb7edb0e
RubarajR <40016173+RubarajR@users.noreply.github.com>
2022-02-01 10:37:58 +0530

    Fixed Edit Page issue in Receipt

--- db/migrate/20220113135023_update_wk_invoice_item.rb
+++ db/migrate/20220113135023_update_wk_invoice_item.rb
@@ -4,6 +4,8 @@ class UpdateWkInvoiceItem < ActiveRecord::Migration[5.2]
     reversible do |dir|
       dir.up do
         change_column :wk_inventory_items, :notes, :text
+        change_column :wk_delivery_items, :notes, :text
+        change_column :wk_material_entries, :comments, :text
       end
     end
     add_column :wk_inventory_items, :invoice_item_id, :integer, default: nil
