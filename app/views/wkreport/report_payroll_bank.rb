710e0e783460834128d9a706f41ab11149bdbd8c
Mohan <nk.mohan96@gmail.com>
2022-02-02 11:18:51 +0530

    Added received quantity filed in invoice
    Add report decrypt values

--- app/views/wkreport/report_payroll_bank.rb
+++ app/views/wkreport/report_payroll_bank.rb
@@ -5,7 +5,7 @@ module ReportPayrollBank
     to = (from >> 1) - 1
     userSqlStr = getUserQueryStr(group_id, user_id, from)
     @userlist = User.find_by_sql(userSqlStr)
-    queryStr = getQueryStr + 			
+    queryStr = getQueryStr +
         "left join groups_users gu on (gu.user_id = u.id and gu.group_id = #{group_id}) " +
         "where u.type = 'User' and component_type != 'c' and (wu.termination_date >= '#{from}' or (u.status = #{User::STATUS_ACTIVE} and wu.termination_date is null))"
     if group_id.to_i > 0 && user_id.to_i < 1
@@ -13,9 +13,9 @@ module ReportPayrollBank
     elsif user_id.to_i > 0
       queryStr = queryStr + " and s.user_id = #{user_id}"
     end
-    
+
     queryStr = queryStr + " and s.salary_date  between '#{from}' and '#{to}' "
-    
+
     if !(validateERPPermission('A_TE_PRVLG') || User.current.admin?)
       queryStr = queryStr + " and u.id = #{User.current.id} "
     end
@@ -29,20 +29,20 @@ module ReportPayrollBank
     @totalhash = Hash.new{|hsh,key| hsh[key] = {} }
     compTotalHash = Hash.new
     last_id = 0
-    totgross = 0 
+    totgross = 0
     totdeduction = 0
     reimbursement = 0
     last_salary_date = 	nil
     syscurrency = Setting.plugin_redmine_wktime['wktime_currency']
-    @salary_data.each do |entry|	
-      @salaryval["#{entry.user_id}"].store "#{entry.component_name}", "#{entry.amount}"	
+    @salary_data.each do |entry|
+      @salaryval["#{entry.user_id}"].store "#{entry.component_name}", "#{entry.amount}"
       if entry.user_id != last_id || entry.salary_date.to_date != last_salary_date
         totgross = 0
         totdeduction = 0
         reimbursement = 0
         last_id = entry.user_id
         last_salary_date = entry.salary_date.to_date
-      end	
+      end
       if compTotalHash[entry.component_name].blank?
         compTotalHash[entry.component_name] = entry.amount
       else
@@ -60,7 +60,7 @@ module ReportPayrollBank
         reimbursement = reimbursement + entry.amount
         compTotalHash['Reimbursements'] = entry.amount + (compTotalHash['Reimbursements'].blank? ? 0 : compTotalHash['Reimbursements'])
       end
-      @totalhash["#{entry.user_id}"].store "gross", "#{totgross}" 
+      @totalhash["#{entry.user_id}"].store "gross", "#{totgross}"
       @totalhash["#{entry.user_id}"].store "deduction", "#{totdeduction}"
       @totalhash["#{entry.user_id}"].store "reimbursement", "#{reimbursement}"
     end
@@ -68,7 +68,7 @@ module ReportPayrollBank
     @rowval = Hash.new{|hsh,key| hsh[key] = {} }
     @userdetails = Hash.new{|hsh,key| hsh[key] = {} }
     count = 1
-    
+
     @userlist.each do |user|
       @userdetails["#{user.id}"].store "#", "#{count}"
       @userdetails["#{user.id}"].store "Name", "#{user.firstname + " " + user.lastname }"
@@ -77,11 +77,11 @@ module ReportPayrollBank
       @headerarr.each do |entry|
         @rowval["#{user.id}"].store "#{entry}", "#{@salaryval["#{user.id}"]["#{entry}"]}"
       end
-      
-      @rowval["#{user.id}"]["#"] = @userdetails["#{user.id}"]["#"] 
+
+      @rowval["#{user.id}"]["#"] = @userdetails["#{user.id}"]["#"]
       @rowval["#{user.id}"]["Name"] = @userdetails["#{user.id}"]["Name"]
       @rowval["#{user.id}"]["Routing Number"] = @userdetails["#{user.id}"]["Routing Number"]
-      @rowval["#{user.id}"]["Account Number"] = @userdetails["#{user.id}"]["Account Number"]
+      @rowval["#{user.id}"]["Account Number"] =decrypt_values(@userdetails["#{user.id}"]["Account Number"])
       @rowval["#{user.id}"]["Net"] = syscurrency.to_s + ((@totalhash["#{user.id}"]["gross"]).to_f - (@totalhash["#{user.id}"]["deduction"]).to_f + (@totalhash["#{user.id}"]["reimbursement"]).to_f).to_s
       count = count + 1
     end
