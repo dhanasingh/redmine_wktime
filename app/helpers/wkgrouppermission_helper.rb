144d7cbe90a9c11c4ef700d7d71e4dc8880e2c4e
eyespot-esp <arun@adhisoftware.co.in>
2024-05-10 21:20:49 +0530

    Added hook for group permissions

--- app/helpers/wkgrouppermission_helper.rb
+++ app/helpers/wkgrouppermission_helper.rb
@@ -1,5 +1,27 @@
 module WkgrouppermissionHelper
+  include WktimeHelper
 
-include WktimeHelper
-
+  def getPermissionModules
+    modules = {
+      "Inventory" => l(:label_inventory),
+      "Shift Scheduling" => l(:label_scheduling),
+      "Survey" => l(:label_survey),
+      "CRM" => l(:label_crm),
+      "Billing" => l(:label_wk_billing),
+      "Accounting" => l(:label_accounting),
+      "Purchase" => l(:label_purchasing),
+      "Report" => l(:label_report),
+      "ATTENDANCE" => l(:report_attendance),
+      "PAYROLL" => l(:label_payroll),
+      "HR" => l(:label_hr),
+      "" => l(:label_general)
+    }
+    addMod = call_hook(:helper_permission_modules)
+		if addMod.present?
+			addMod = eval(addMod)
+      puts addMod
+      modules =  modules.merge(addMod)
+    end
+    modules
+  end
 end
