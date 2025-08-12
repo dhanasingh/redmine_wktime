1a302494c00f387d5a5bad5f90c9bedae9a40893
Jean-François Dessureault <jfdessureault@arcanetech.io>
2025-08-12 14:18:18 -0400

    Fixed invalid SQL syntax in 20250723135555_update_crm_activity_statuses.rb

--- db/migrate/20250723135555_update_crm_activity_statuses.rb
+++ db/migrate/20250723135555_update_crm_activity_statuses.rb
@@ -1,22 +1,13 @@
 class UpdateCrmActivityStatuses < ActiveRecord::Migration[7.2]
   def up
-    execute <<-SQL
-      UPDATE wk_crm_activities SET status = 'NS' WHERE status = 'P';
-      UPDATE wk_crm_activities SET status = 'C'  WHERE status = 'H';
-      UPDATE wk_crm_activities SET status = 'D'  WHERE status = 'NH';
-    SQL
+    execute "UPDATE wk_crm_activities SET status = 'NS' WHERE status = 'P'"
+    execute "UPDATE wk_crm_activities SET status = 'C'  WHERE status = 'H'"
+    execute "UPDATE wk_crm_activities SET status = 'D'  WHERE status = 'NH'"
   end
 
   def down
-    execute <<-SQL
-      UPDATE wk_crm_activities SET status = 'P'
-      WHERE status = 'NS' AND activity_type IN ('C', 'M');
-
-      UPDATE wk_crm_activities SET status = 'H'
-      WHERE status = 'C' AND activity_type IN ('C', 'M');
-
-      UPDATE wk_crm_activities SET status = 'NH'
-      WHERE status = 'D' AND activity_type IN ('C', 'M');
-    SQL
+    execute "UPDATE wk_crm_activities SET status = 'P' WHERE status = 'NS' AND activity_type IN ('C', 'M')"
+    execute "UPDATE wk_crm_activities SET status = 'H' WHERE status = 'C' AND activity_type IN ('C', 'M')"
+    execute "UPDATE wk_crm_activities SET status = 'NH' WHERE status = 'D' AND activity_type IN ('C', 'M')"
   end
 end
