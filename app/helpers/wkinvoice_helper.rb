fe2db16ebc81b07b26bd69b2b8b1fcb86c183ad3
RubarajR <rubaraj1093@gmail.com>
2026-06-19 14:14:39 +0530

    Fixed evaluation points billing issue

--- app/helpers/wkinvoice_helper.rb
+++ app/helpers/wkinvoice_helper.rb
@@ -356,13 +356,17 @@ include WkpayrollHelper
 					end
 				end
 				invItem = @invoice.invoice_items.new()
+				itemRateHash = rateHash
 				if accountProject.itemized_bill
+					rate_override = Redmine::Hook.call_hook(:get_entry_billing_rate, { entry: entry, rate_hash: rateHash, invoice: @invoice, account_project: accountProject })
+					rate_override = rate_override.compact.first if rate_override.present?
+					itemRateHash = rate_override if rate_override.present? && rate_override['rate'].present?
 					pjtDescription =  entry.issue.blank? ? entry.project.name : (isAccountBilling(accountProject) ? entry.project.name + ' - ' + entry.issue.subject : entry.issue.subject)
 					pjtQuantity = sumEntry[entry.issue_id]
-					qty_override = Redmine::Hook.call_hook(:get_entry_billing_quantity, { entry: entry, rate_hash: rateHash, invoice: @invoice, account_project: accountProject })
+					qty_override = Redmine::Hook.call_hook(:get_entry_billing_quantity, { entry: entry, rate_hash: itemRateHash, invoice: @invoice, account_project: accountProject })
 					pjtQuantity = qty_override.compact.first if qty_override.present? && qty_override.compact.first.present?
-					amount = rateHash['rate'] * pjtQuantity
-					invItem = updateInvoiceItem(invItem, accountProject.project_id, pjtDescription, rateHash['rate'], pjtQuantity, rateHash['currency'], 'i', amount, nil, nil, nil, 'Issue', entry&.issue_id) unless isCreate
+					amount = itemRateHash['rate'] * pjtQuantity
+					invItem = updateInvoiceItem(invItem, accountProject.project_id, pjtDescription, itemRateHash['rate'], pjtQuantity, itemRateHash['currency'], 'i', amount, nil, nil, nil, 'Issue', entry&.issue_id) unless isCreate
 				else
 					isContinue = true
 					pjtQuantity = timeEntries.sum(:hours)
@@ -378,12 +382,12 @@ include WkpayrollHelper
 				end
 				pjtIdVal << entry.id
     			if isCreate && (oldIssueId == 0 || (oldIssueId != entry.issue_id && accountProject.itemized_bill)) # need to add accountProject.itemized_billcheck to avoid duplicate entries on preview billing
-					itemAmount = rateHash['rate'] * pjtQuantity
+					itemAmount = itemRateHash['rate'] * pjtQuantity
 					@invItems[@itemCount].store 'project_id', accountProject.project_id
 					@invItems[@itemCount].store 'item_desc', pjtDescription
 					@invItems[@itemCount].store 'item_type', 'i'
-					@invItems[@itemCount].store 'rate', rateHash['rate']
-					@invItems[@itemCount].store 'currency', rateHash['currency']
+					@invItems[@itemCount].store 'rate', itemRateHash['rate']
+					@invItems[@itemCount].store 'currency', itemRateHash['currency']
 					@invItems[@itemCount].store 'item_quantity', pjtQuantity.round(4)
 					@invItems[@itemCount].store 'item_amount', itemAmount.round(2)
 					@invItems[@itemCount].store 'issue_id', accountProject.itemized_bill ? entry.issue_id : 0
